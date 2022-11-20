05981f0fa055db452b5141692bf6f53b11b92529
Pavel_D3v <179werpavel@protonmail.com>
2022-11-20 21:52:33 +0300

    endpoint update

--- TodoCursuch/back/index.js
+++ TodoCursuch/back/index.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
-
+app.get("/", (req, res) => {
+    res.send("Hello from dagestan")
+});
 (async () => {
     try {
         mongoose.connect(
